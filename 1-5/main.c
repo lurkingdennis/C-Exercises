48c47331536aacf55edd47311751e405a4b174e3
Dennis C <dpclements@proton.me>
2023-10-16 20:39:34 -0400

    update 1-5 answer

--- 1-5/main.c
+++ 1-5/main.c
@@ -2,23 +2,21 @@
 
 
 /* Modify the temperature program to print the table in reverse order, that is, from 300 degrees to 0. */
-int main(int argc, char *argv[])
-{
-	float celcius, fahr;
+int main(int argc, char *argv[]) {
+
+	int celcius;
 
 	float upper, lower, step;
 
 	upper = 300; /* upper limit */
 	lower = 0; /* lower limit */
-	step = -20; /* step size */
+	step = 20; /* step down size */
 
 	celcius = upper; 
 
 	printf("Celcius\tFahrenheit\n");
-	while(celcius >= lower) {
-		fahr = (celcius * (9.0/5.0)) + 32.0;
-		printf("%3.0f\t%6.0f\n", celcius, fahr);
-		celcius = celcius + step;
+	for(celcius = upper; celcius >= lower; celcius = celcius - step) {
+		printf("%d\t%6.0f\n", celcius, (celcius * (9.0/5.0) + 32.0));
 	}
 
 	return 0;
