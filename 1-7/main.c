9bdb4b615d87a1945520f61564f6b8310cf7177e
Dennis C <dpclements@proton.me>
2023-10-16 21:29:55 -0400

    updated comment

--- 1-7/main.c
+++ 1-7/main.c
@@ -3,7 +3,7 @@
 /* Write a program to print the value of EOF */
 int main(int argc, char *argv[])
 {
-	// prints the value of EOF which is 1
+	// prints the value of EOF which is -1
 	printf("the value of EOF: %d\n", EOF);
 	return 0;
 }
