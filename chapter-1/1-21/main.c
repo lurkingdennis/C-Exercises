d857479654b0af406db76574a22f242fd964c99e
Dennis C <dpclements@proton.me>
2023-11-09 16:27:46 -0800

    solution for 1-21

--- chapter-1/1-21/main.c
+++ chapter-1/1-21/main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
 
-#define TABS 8
+#define TABS 6
 
+/* Write a program entab that replaces strings of blanks by the minimum number of tabs and blanks to achieve the same spacing. Use the same tab stop, which should be given preference? */
 int main(int argc, char *argv[])
 {
   int c;
-  int nr_of_spaces = 0;
+  int spacecount = 0;
   while ( ( c = getchar() ) != EOF ) {
     if ( c == ' ' ) {
-      ++nr_of_spaces;
-      if ( nr_of_spaces % TABS == 0 ) {
+      ++spacecount;
+      if ( spacecount % TABS == 0 ) {
         putchar('\t');
+        spacecount = 0;
       }
     } else {
-      while(nr_of_spaces) {
+      while ( spacecount > 0 ) {
         putchar(' ');
-        --nr_of_spaces;
+        --spacecount;
       }
       putchar(c);
     }
